bb1a62295a83b30545faba79f8b62185f917aea1
renou <renou@users.noreply.github.com>
2020-10-19 22:39:24 -0700

    basic ticker model

--- roll/option/models.py
+++ roll/option/models.py
@@ -2,6 +2,28 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class BaseModel(models.Model):
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True  # specify this model as an Abstract Model
+        app_label = 'option'
+
+
+class Ticker(BaseModel):
+    symbol = models.CharField(max_length=20, null=True, blank=True)
+    full_name = models.CharField(max_length=200, null=True, blank=True)
+    STATUS_CHOICES = (
+        ("unspecified", "Unspecified"),
+        ("disabled", "Disabled"),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="unspecified")
+
+    def __str__(self):
+        return str(self.symbol)
+
+
 class User(AbstractUser):
     class Meta:
         app_label = 'option'
