59acd809cfa34d6fd2376329718ddf0d4391e9ed
Nanyou Guan <nanyouguan@gmail.com>
2020-10-21 22:42:55 -0400

    Initial frontend commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
