f0249d9e4565ee9cf35f41c42e499028f0de573b
renou <renou@users.noreply.github.com>
2020-11-01 15:39:32 -0800

    add GA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 import './App.css';
 import Home from './pages/Home';
 import Disclaimer from './components/disclaimer'
@@ -12,8 +14,16 @@ import Header from './components/header'
 import Footer from './components/footer'
 
 function App() {
+  const history = createBrowserHistory();
+  // Initialize google analytics page view tracking
+  history.listen(location => {
+    ReactGA.initialize('G-3LMHS8L17E');
+    ReactGA.set({ page: location.pathname }); // Update the user's current page
+    ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  });
+
   return (
-    <Router>
+    <Router history={history}>
       <Header></Header>
       <div class="container">
         <Switch>
