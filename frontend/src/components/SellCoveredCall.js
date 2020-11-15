febb061e6de2bd52b70226e8d43f767ef76028a5
renou <renou@users.noreply.github.com>
2020-11-15 10:45:11 -0800

    added lib

--- frontend/src/components/SellCoveredCall.js
+++ frontend/src/components/SellCoveredCall.js
@@ -198,7 +198,7 @@ export default function SellCoveredCall() {
                             })}
                             noDataIndication="No Data"
                             bordered={false}
-                            // overlay={overlayFactory({ spinner: true })} does not work with filter.
+                            // overlay={overlayFactory({ spinner: true })} // does not work with filter.
                             expandRow={ExpandContractRow()}
                             rowStyle={InTheMoneyRowStyle}
                             filter={filterFactory()}
