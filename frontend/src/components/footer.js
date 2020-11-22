ca1994d3191d1b6ec36cce627f7e4f0aea6b7053
renou <renou@users.noreply.github.com>
2020-11-21 16:50:37 -0800

    fix footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -4,18 +4,27 @@ import { Link } from "react-router-dom"
 function Footer() {
     return (
         <footer className="border-top align-items-center p-3 px-md-4 mb-3">
-            <div className="row">
-                <div className="col">
-                    <p className="text-muted">
-                        All Content on tigerstance.com is for informational purposes only, you should not construe
-                        any such information or other material as legal, tax, investment, financial, or other advice.
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <p className="text-muted">
+                            All content and data on tigerstance.com is for informational purposes only, you should not construe
+                            any such information or other material as legal, tax, trading, investment, financial, or other advice.
                         See <Link to="/disclaimer" > full legal disclaimer</Link>.
                     </p>
+                    </div>
                 </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <small className="d-block mb-3 text-muted">Copyright &copy; 2020 tigerstance.com. All Rights Reserved.</small>
+                <div className="row">
+                    <div className="col">
+                        <p className="text-muted">
+                            Data on tigerstance.com is delayed and may not reflect the latest market condition.
+                        </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <small className="d-block mb-3 text-muted">Copyright &copy; 2020 tigerstance.com. All Rights Reserved.</small>
+                    </div>
                 </div>
             </div>
         </footer>
