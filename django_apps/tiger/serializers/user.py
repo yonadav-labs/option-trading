920873f0ddbc55a9484590f33ab5a57168318be8
Nanyou Guan <nanyouguan@gmail.com>
2020-12-01 17:19:13 -0500

    remove is_subscriber and watchlist from user model

--- django_apps/tiger/serializers/user.py
+++ django_apps/tiger/serializers/user.py
@@ -5,5 +5,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'id', 'okta_id', 'is_subscriber', 'first_name', 'last_name', 'is_superuser',
-                  'is_staff', 'is_active', 'watchlist')
+        fields = ('username', 'email', 'id', 'okta_id','first_name', 'last_name')
