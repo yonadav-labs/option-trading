d26a0447f4d68ed21f2216031e0d366d32d4d61c
renou <renou@users.noreply.github.com>
2020-12-02 23:02:36 -0800

    not use fix navbar

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -48,21 +48,13 @@ function Header() {
 
     return (
         <header>
-            <Navbar className="fixed-top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand as={Link} to="/">Tigerstance</Navbar.Brand>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand as={Link} to="/" className="font-weight-bold">Tigerstance</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link as={Link} to="/">Home</Nav.Link>
                         <Nav.Link as={Link} to="/buy-call">Buy call</Nav.Link>
                         <Nav.Link as={Link} to="/sell-covered-call">Sell covered call</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
                     </Nav>
                     {authState.isAuthenticated ? <Nav><Nav.Link as={Link} to="/profile">Profile</Nav.Link></Nav> : null}
                     <Nav>
