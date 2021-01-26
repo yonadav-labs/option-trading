723990d5c1be490f5800ccac2e2ba07e34521b89
renou <renou@users.noreply.github.com>
2021-01-25 21:30:20 -0800

    fix filter_low_liquidity bug

--- django_apps/tiger/views/utils.py
+++ django_apps/tiger/views/utils.py
@@ -2,14 +2,17 @@ def is_low_liquidity(contract):
     return contract.open_interest <= 10 or contract.volume <= 1
 
 
-def get_valid_contracts(ticker, request, use_as_premium, all_expiration_timestamps, filter_low_liquidity):
+def get_valid_contracts(ticker, request, use_as_premium, all_expiration_timestamps, filter_low_liquidity=False):
     input_expiration_timestamps = set([int(ts) for ts in request.query_params.getlist('expiration_timestamps') if
                                        int(ts) in all_expiration_timestamps])
     call_lists = []
     put_lists = []
     for ts in input_expiration_timestamps:
         calls, puts = ticker.get_call_puts(use_as_premium, ts)
+        if filter_low_liquidity:
+            calls = filter(lambda call: not is_low_liquidity(call), calls)
+            puts = filter(lambda put: not is_low_liquidity(put), puts)
         # filter out inactive contracts.
-        call_lists.append(list(filter(lambda call: call.last_trade_date and not is_low_liquidity(call), calls)))
-        put_lists.append(list(filter(lambda put: put.last_trade_date and not is_low_liquidity(put), puts)))
+        call_lists.append(list(filter(lambda call: call.last_trade_date, calls)))
+        put_lists.append(list(filter(lambda put: put.last_trade_date, puts)))
     return call_lists, put_lists
