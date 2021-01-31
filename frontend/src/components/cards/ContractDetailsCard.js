0e473ace3c6fc98171d4087f40588c2d5ba94d04
renou <renou@users.noreply.github.com>
2021-01-31 12:42:29 -0800

    Update ContractDetailsCard.js

--- frontend/src/components/cards/ContractDetailsCard.js
+++ frontend/src/components/cards/ContractDetailsCard.js
@@ -1,71 +1,112 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
+import React, { useState } from 'react';
+import { Row, Col, Badge, Card } from 'react-bootstrap';
 
 import {
     PriceFormatter, PercentageFormatter, TimestampTimeFormatter,
-    NumberRoundFormatter, TimestampDateFormatter, PriceMovementFormatter
+    NumberRoundFormatter
 } from '../../utils';
 
 export default function ContractDetailsCard(props) {
     const { contract, hideTitle } = props;
-
+    const [readMore, setReadMore] = useState(false);
+    const linkName = readMore ? 'Collapse <<' : 'More >>'
     return (
         (
             contract ?
-            <Card>
-                {
-                    hideTitle ?
-                        null : <Card.Header>{contract.display_name}</Card.Header>
-                    // null :
-                    // <Card.Header>
-                    //     <Row>
-                    //         <Col>{contract.is_call ? 'Call' : 'Put'}</Col>
-                    //         <Col>Expiration: {TimestampDateFormatter(contract.expiration)}</Col>
-                    //         <Col>Strike: {PriceFormatter(contract.strike)}</Col>
-                    //         {/* <Col>Bid: {PriceFormatter(contract.bid)}</Col> */}
-                    //         <Col>Premium: {PriceFormatter(contract.premium)} <br/><small>Bid: {PriceFormatter(contract.bid)} Ask: {PriceFormatter(contract.ask)}</small></Col>
-                    //         {/* <Col>Ask: {PriceFormatter(contract.ask)}</Col> */}
-                    //         <Col>Breakeven: {PriceMovementFormatter(contract.to_break_even_ratio, contract.break_even_price)}</Col>
-                    //     </Row>
-                    // </Card.Header>
-                }
-                <Card.Body>
-                    <Card.Text>
-                        <div>
-                            <div className="row">
-                                <div className="col-sm-3">Last: {PriceFormatter(contract.last_price)}</div>
-                                <div className="col-sm-3">Bid: {PriceFormatter(contract.bid)} X {NumberRoundFormatter(contract.bid_size)}</div>
-                                <div className="col-sm-3">Ask: {PriceFormatter(contract.ask)} X {NumberRoundFormatter(contract.ask_size)}</div>
-                                <div className="col-sm-3">Quoted at: {TimestampTimeFormatter(contract.quote_time)}</div>
-                            </div>
-                            <div className="row">
-                                <div className="col-sm-3">Change: {PriceFormatter(contract.change)} ({NumberRoundFormatter(contract.percent_change)}%)</div>
-                                <div className="col-sm-3">Range: {PriceFormatter(contract.low_price)} - {PriceFormatter(contract.high_price)}</div>
-                                <div className="col-sm-3">Implied volatility: {PercentageFormatter(contract.implied_volatility)}</div>
-                                <div className="col-sm-3"></div>
-                            </div>
-                            <div className="row">
-                                <div className="col-sm-3">Open interest: {contract.open_interest}</div>
-                                <div className="col-sm-3">Volume: {contract.volume}</div>
-                                <div className="col-sm-3">Last traded: {TimestampTimeFormatter(contract.last_trade_date)}</div>
-                                <div className="col-sm-3">To expiration: {contract.days_till_expiration} days</div>
-                            </div>
-                            <div className="row">
-                                <div className="col-sm-3">Delta: {NumberRoundFormatter(contract.delta)}</div>
-                                <div className="col-sm-3">Gamma: {NumberRoundFormatter(contract.gamma)}</div>
-                                <div className="col-sm-3">Theta: {NumberRoundFormatter(contract.theta)}</div>
-                                <div className="col-sm-3">Vega: {NumberRoundFormatter(contract.vega)}</div>
-                            </div>
-                            <div className="row">
-                                <div className="col-sm-3">Rho: {NumberRoundFormatter(contract.rho)}</div>
-                                <div className="col-sm-3">Break-even at: {PriceFormatter(contract.break_even_price)}</div>
-                            </div>
-                        </div>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-            :
-            null
+                <Card>
+                    {hideTitle ? null : <Card.Header>{contract.display_name}</Card.Header>}
+                    <Card.Body>
+                        <Card.Text>
+                            <Row>
+                                <Col sm="3" xs="6">
+                                    <Badge variant="secondary">Premium</Badge>
+                                    <div>{PriceFormatter(contract.premium)}</div>
+                                </Col>
+                                <Col sm="3" xs="6">
+                                    <Badge variant="secondary">Bid</Badge>
+                                    <div>{PriceFormatter(contract.bid)} X {NumberRoundFormatter(contract.bid_size)}</div>
+                                </Col>
+                                <Col sm="3" xs="6">
+                                    <Badge variant="secondary">Ask</Badge>
+                                    <div>{PriceFormatter(contract.ask)} X {NumberRoundFormatter(contract.ask_size)}</div>
+                                </Col>
+                                <Col sm="3" xs="6">
+                                    <Badge variant="secondary">Last</Badge>
+                                    <div>{PriceFormatter(contract.last_price)}</div>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col sm="3" xs="6">
+                                    <Badge variant="secondary">Change</Badge>
+                                    <div>{PriceFormatter(contract.change)} ({NumberRoundFormatter(contract.percent_change)}%)</div>
+                                </Col>
+                                <Col sm="3" xs="6">
+                                    <Badge variant="secondary">Range</Badge>
+                                    <div>{PriceFormatter(contract.low_price)} - {PriceFormatter(contract.high_price)}</div>
+                                </Col>
+                                <Col sm="3" xs="6">
+                                    <Badge variant="secondary">Open interest</Badge>
+                                    <div>{contract.open_interest}</div>
+                                </Col>
+                                <Col sm="3" xs="6">
+                                    <Badge variant="secondary">Volume</Badge>
+                                    <div>{contract.volume}</div>
+                                </Col>
+                            </Row>
+                            {readMore ? <div>
+                                <Row>
+                                    <Col sm="3" xs="6">
+                                        <Badge variant="secondary">Delta</Badge>
+                                        <div>{NumberRoundFormatter(contract.delta)}</div>
+                                    </Col>
+                                    <Col sm="3" xs="6">
+                                        <Badge variant="secondary">Gamma</Badge>
+                                        <div>{NumberRoundFormatter(contract.gamma)}</div>
+                                    </Col>
+                                    <Col sm="3" xs="6">
+                                        <Badge variant="secondary">Theta</Badge>
+                                        <div>{NumberRoundFormatter(contract.theta)}</div>
+                                    </Col>
+                                    <Col sm="3" xs="6">
+                                        <Badge variant="secondary">Vega</Badge>
+                                        <div>{NumberRoundFormatter(contract.vega)}</div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col sm="3" xs="6">
+                                        <Badge variant="secondary">Rho</Badge>
+                                        <div>{NumberRoundFormatter(contract.rho)}</div>
+                                    </Col>
+                                    <Col sm="3" xs="6">
+                                        <Badge variant="secondary">Implied volatility</Badge>
+                                        <div>{PercentageFormatter(contract.implied_volatility)}</div>
+                                    </Col>
+                                    <Col sm="3" xs="6">
+                                        <Badge variant="secondary">Break-even at</Badge>
+                                        <div>{PriceFormatter(contract.break_even_price)}</div>
+                                    </Col>
+                                    <Col sm="3" xs="6">
+                                        <Badge variant="secondary">To expiration</Badge>
+                                        <div>{contract.days_till_expiration} days</div>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col sm="3" xs="12">
+                                        <Badge variant="secondary">Last traded</Badge>
+                                        <div>{TimestampTimeFormatter(contract.last_trade_date)}</div>
+                                    </Col>
+                                    <Col sm="3" xs="12">
+                                        <Badge variant="secondary">Quoted at</Badge>
+                                        <div>{TimestampTimeFormatter(contract.quote_time)}</div>
+                                    </Col>
+                                </Row>
+                                <a className="read-more-link" style={{ cursor: 'pointer' }} onClick={() => { setReadMore(!readMore) }}>{linkName}</a>
+                            </div> : <a className="read-more-link" style={{ cursor: 'pointer' }} onClick={() => { setReadMore(!readMore) }}>{linkName}</a>}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                :
+                null
         )
     );
 }
