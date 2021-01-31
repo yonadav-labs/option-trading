5f487e488ee048d7f639ff73054f288869498b29
renou <renou@users.noreply.github.com>
2021-01-30 22:39:32 -0800

    update homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,42 +1,11 @@
 import React from 'react';
-// import Carousel from "react-bootstrap/Carousel";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { Row, Col } from 'react-bootstrap';
 import './Home.css';
 
 export default function Home() {
     return (
         <div>
-            {/* <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block"
-                        src="/images/home/home-banner-1.jpg"
-                    />
-                    <Carousel.Caption>
-                        <h1>We do the math, so you don't have to.</h1>
-                        <p>Understand option contracts in one glance.</p>
-                        <p>
-                            <a class="btn btn-lg btn-primary mr-3" href="signin/register" role="button">SIGN UP</a>
-                            <a class="btn btn-lg btn-secondary ml-3" href="#" role="button">OUR PRODUCT</a>
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="img-responsive d-block"
-                        src="/images/home/home-banner-2.jpg"
-                    />
-                    <Carousel.Caption>
-                        <h1>Never get lost in the option chain again.</h1>
-                        <p>Find the right contract just for you.</p>
-                        <p>
-                            <a class="btn btn-lg btn-primary mr-3" href="signin/register" role="button">SIGN UP</a>
-                            <a class="btn btn-lg btn-secondary ml-3" href="#" role="button">OUR PRODUCT</a>
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel> */}
-
             <div className="container-fluid home-background">
                 {/* <img src={HeroRight} alt="hero-right" /> */}
                 <div className="row p-5 min-vh-100">
@@ -52,100 +21,115 @@ export default function Home() {
                     </div>
                 </div>
 
-                <div className="row p-5 min-vh-100">
+                <Row className="p-5">
                     <div className="col-lg-12 text-center my-auto">
-                        <h4 className="text-primary">Our Mission</h4>
-                        <h2>Tigerstance helps investors like you to find and understand options trading opportunities.</h2>
-                    </div>
-                    <div className="col-lg-3 text-center">
-                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
-                        <h2>Long Call</h2>
-                        <p>Find the best call options to buy.</p>
-                        <p>
-                            <Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link>
-                        </p>
-                    </div>
-                    <div className="col-lg-3 text-center">
-                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
-                        <h2>Covered Call</h2>
-                        <p>Explore covered calls to sell.</p>
-                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
-                    </div>
-                    <div className="col-lg-3 text-center">
-                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
-                        <h2>Long Put</h2>
-                        <p>Find the best puts to buy.</p>
-                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
-                    </div>
-                    <div className="col-lg-3 text-center">
-                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
-                        <h2>Cash Secured Put</h2>
-                        <p>Explore cash secured puts to sell.</p>
-                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
-                    </div>
-                    <div className="col-lg-3 text-center">
-                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
-                        <h2>Bull call spread</h2>
-                        <p>Explore the best bull call spread to buy.</p>
-                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
-                    </div>
-                    <div className="col-lg-3 text-center">
-                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
-                        <h2>More strategies coming...</h2>
-                        <p>Stay tuned.</p>
-                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
-                    </div>
-                </div>
-
-                <div className="row p-5 min-vh-100">
-                    <div className="col-lg-12 text-center my-auto">
-                        <h4 className="text-primary">How it Works</h4>
+                        <h4 className="text-primary">Strategy Screener: how it works</h4>
                         <h1 className="find-title">Find the right options trading ideas just for you</h1>
                     </div>
                     <div className="col-lg-5 mb-3">
                         <img className="img-fluid mx-auto d-block" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="400" height="400" />
                     </div>
                     <div className="col-lg-7">
-                        <div className="row">
+                        <Row>
                             <span className="col-lg-1">
                                 <h2 className="display-4 font-weight-bold">1</h2>
                             </span>
                             <span className="col-lg-11">
-                                <h2>Search for your stock ticker</h2>
-                                <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius, diam vitae.</p>
+                                <h2>Select a stock by its ticker.</h2>
+                                <p className="lead">AAPL, AMZN, TSLA...</p>
                             </span>
-                        </div>
-                        <div className="row">
+                        </Row>
+                        <Row>
                             <span className="col-lg-1">
                                 <h2 className="display-4 font-weight-bold text-muted">2</h2>
                             </span>
                             <span className="col-lg-11">
-                                <h2 className="text-muted">Select a expiration date</h2>
-                                <p className="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius, diam vitae.</p>
+                                <h2 className="text-muted">Select an expiration date.</h2>
+                                <p className="lead text-muted">Timeframe for this trade.</p>
                             </span>
-                        </div>
-                        <div className="row">
+                        </Row>
+                        <Row>
                             <span className="col-lg-1">
                                 <h2 className="display-4 font-weight-bold text-muted">3</h2>
                             </span>
                             <span className="col-lg-11">
-                                <h2 className="text-muted">Enter your target price</h2>
-                                <p className="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius, diam vitae.</p>
+                                <h2 className="text-muted">Enter target stock price.</h2>
+                                <p className="lead text-muted">Where do you think the stock price will go?</p>
                             </span>
-                        </div>
-                        <div className="row">
+                        </Row>
+                        <Row>
                             <span className="col-lg-1">
                                 <h2 className="display-4 font-weight-bold text-muted">4</h2>
                             </span>
                             <span className="col-lg-11">
-                                <h2 className="text-muted">Get the trading ideas</h2>
-                                <p className="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius, diam vitae.</p>
+                                <h2 className="text-muted">Get trading ideas.</h2>
+                                <p className="lead text-muted">We enumerate tens of thousands trading ideas and present the best ones to you.</p>
                             </span>
-                        </div>
+                        </Row>
                     </div>
-                </div>
+                </Row>
 
-                <div className="row p-5 justify-content-center min-vh-100">
+                <Row className="p-5">
+                    <Col lg="12" className="text-center my-auto">
+                        <h3 className="text-primary">Supported options strategies</h3>
+                        <br />
+                    </Col>
+                    <Col lg="2" xs="6" className="text-center">
+                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
+                        <h4>Long Call</h4>
+                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
+                    </Col>
+                    <Col lg="2" xs="6" className="text-center">
+                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
+                        <h4>Covered Call</h4>
+                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
+                    </Col>
+                    <Col lg="2" xs="6" className="text-center">
+                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
+                        <h4>Long Put</h4>
+                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
+                    </Col>
+                    <Col lg="2" xs="6" className="text-center">
+                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
+                        <h4>Cash Secured Put</h4>
+                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
+                    </Col>
+                    <Col lg="2" xs="6" className="text-center">
+                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
+                        <h4>Bull call spread</h4>
+                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
+                    </Col>
+                    <Col lg="2" xs="6" className="text-center">
+                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
+                        <h4>Bear call spread</h4>
+                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
+                    </Col>
+                    <Col lg="2" xs="6" className="text-center">
+                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
+                        <h4>Bear put spread</h4>
+                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
+                    </Col>
+                    <Col lg="2" xs="6" className="text-center">
+                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
+                        <h4>Bull put spread</h4>
+                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
+                    </Col>
+                    <Col lg="2" xs="6" className="text-center">
+                        <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="100" height="100" />
+                        <h4>More coming...</h4>
+                        <p><Link className="btn btn-secondary" to="/strategy-screener" >Get Started</Link></p>
+                    </Col>
+                </Row>
+
+
+                <Row className="p-5">
+                    <Col lg="12" className="text-center my-auto">
+                        <h4 className="text-primary">Our Mission</h4>
+                        <h2>Tigerstance helps investors like you to find and understand options trading opportunities.</h2>
+                    </Col>
+                </Row>
+
+                <Row className="p-5 justify-content-center min-vh-100">
                     <div className="col-lg-12 text-center my-auto">
                         <h4 className="text-primary">Testimonials</h4>
                         <h1>Our Customers Love Us</h1>
@@ -153,7 +137,7 @@ export default function Home() {
                     <div className="col-lg-12 text-center">
                         <h2>“Tigerstance helps me make decisions with ease and confidence in this volatile market.”</h2>
                     </div>
-                    <div className="row col-lg-6 justify-content-center">
+                    {/* <div className="row col-lg-6 justify-content-center">
                         <div className="col-lg-2 text-center">
                             <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="70" height="70" />
                         </div>
@@ -163,8 +147,8 @@ export default function Home() {
                         <div className="col-lg-2 text-center">
                             <img className="rounded-circle mb-4" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="70" height="70" />
                         </div>
-                    </div>
-                </div>
+                    </div> */}
+                </Row>
             </div>
         </div>
     );
