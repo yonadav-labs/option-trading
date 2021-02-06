fede659adb7e7211dfc86ef041f092a16b9ee1d1
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2021-02-06 11:25:17 -0800

    Don/auth (#341)
    * PEP8
    
    * load first_name, last_name of user
    
    * add verified email page
    
    * revise verification email page
    
    * integrate facebook
    
    * revise
    
    * separate okta account for dev
    
    * integrate google
    
    * implement redirect to the current page
    
    * revise spacing
    
    * handle various cases for redirect
    
    * debug registration
    
    * handle redirect from verify-email

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,66 +1,66 @@
-import React, { useEffect } from "react";
-import * as OktaSignIn from "@okta/okta-signin-widget";
-import "@okta/okta-signin-widget/dist/css/okta-sign-in.min.css";
+import React, { useEffect } from 'react';
+import { useOktaAuth } from '@okta/okta-react';
+import * as OktaSignIn from '@okta/okta-signin-widget';
+import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
 
 import getOktaConfig from "./../oktaConfig";
 
-export default function Login() {
-    const { pkce, issuer, clientId, redirectUri, scopes, idpFbId } = getOktaConfig().oidc;
-    const signIn = new OktaSignIn({
-        baseUrl: issuer.split("/oauth2")[0],
-        clientId,
-        redirectUri,
-        logo: "/logo192.png",
-        i18n: {
-            en: {
-                'primaryauth.title': 'Sign in Tigerstance'
-            }
-        },
-        authParams: {
-            pkce,
-            issuer,
-            display: 'page',
-            scopes
-        },
-        idps: [
-            {type: 'FACEBOOK', id: idpFbId}
-        ],
-        idpDisplay: 'SECONDARY',
-        // registration: {
-        //     parseSchema: function (schema, onSuccess, onFailure) {
-        //         // handle parseSchema callback
-        //         onSuccess(schema);
-        //     },
-        //     preSubmit: function (postData, onSuccess, onFailure) {
-        //         // handle preSubmit callback
-        //         onSuccess(postData);
-        //     },
-        //     postSubmit: function (response, onSuccess, onFailure) {
-        //         // handle postsubmit callback
-        //         onSuccess(response);
-        //     }
-        // },
-        features: { registration: true, router: true }
-    });
+const Login = () => {
+    const { authService } = useOktaAuth();
 
     useEffect(() => {
-        signIn.renderEl(
-            { el: '#sign-in-widget' },
-            () => { },
-            err => {
-                throw err;
-            }
-        );
+        const { pkce, issuer, clientId, redirectUri, scopes, idpFbId, idpGlId } = getOktaConfig().oidc;
+        const widget = new OktaSignIn({
+            baseUrl: issuer.split('/oauth2')[0],
+            clientId,
+            redirectUri,
+            logo: "/logo192.png",
+            i18n: {
+                en: {
+                    'primaryauth.title': 'Sign in Tigerstance'
+                },
+            },
+            authParams: {
+                // To avoid redirect do not set "pkce" or "display" here. OKTA-335945
+                issuer,
+                scopes,
+            },
+            idps: [
+                {type: 'FACEBOOK', id: idpFbId},
+                {type: 'GOOGLE', id: idpGlId},
+            ],
+            idpDisplay: 'SECONDARY',
+            features: { registration: true, router: true }
+        });
 
-        // returned function will be called on component unmount 
-        return () => {
-            signIn.remove();
-        }
-    }, [])
+        widget.renderEl(
+            { el: '#sign-in-widget' },
+            ({ tokens }) => {
+                if (tokens) {
+                    // Add tokens to storage
+                    const tokenManager = authService.getTokenManager();
+                    tokenManager.add('idToken', tokens.idToken);
+                    tokenManager.add('accessToken', tokens.accessToken);
+
+                    // Return to the original URL (if auth was initiated from a secure route), falls back to the origin
+                    const fromUri = authService.getFromUri();
+                    if (document.referrer === "" || document.referrer.includes("signin") || document.referrer.includes("verify-email")) {
+                        window.location.assign(fromUri);
+                    } else {
+                        window.location.assign(document.referrer);
+                    }
+                }
+            },
+            (err) => {
+                throw err;
+            },
+        );
+    }, [authService]);
 
     return (
         <div className="min-vh-100">
             <div id="sign-in-widget" />
         </div>
     );
-}
+};
+export default Login;
--- frontend/src/oktaConfig.js
+++ frontend/src/oktaConfig.js
@@ -1,4 +1,4 @@
-const { REACT_APP_OIDC_CLIENT_ID, REACT_APP_OIDC_ISSUER, REACT_APP_OIDC_REDIRECT_URI, REACT_APP_IDP_FB_ID } = process.env;
+const { REACT_APP_OIDC_CLIENT_ID, REACT_APP_OIDC_ISSUER, REACT_APP_OIDC_REDIRECT_URI, REACT_APP_IDP_FB_ID, REACT_APP_IDP_GL_ID } = process.env;
 
 export default function getOktaConfig() {
   return {
@@ -7,6 +7,7 @@ export default function getOktaConfig() {
       issuer: REACT_APP_OIDC_ISSUER,
       redirectUri: REACT_APP_OIDC_REDIRECT_URI,
       idpFbId: REACT_APP_IDP_FB_ID,
+      idpGlId: REACT_APP_IDP_GL_ID,
       scopes: ['openid', 'profile', 'email'],
       pkce: true
     }
