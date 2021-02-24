5bc19122f74f0ea3d95f87a537e45dee08b1bc3e
shakilabdin <56568494+shakilabdin@users.noreply.github.com>
2021-02-24 12:56:28 -0500

    Twitter timeline (#459)
    * added timeline maybe change position
    
    * added timeline maybe change position
    
    * undo package
    
    * moved to footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,20 +1,33 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap';
 import { Link } from "react-router-dom"
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
 
 function Footer(props) {
     return (
         <div className={`container-fluid ${props.class}`} style={{ background: '#2A2A2A' }} >
             {/* <hr className="featurette-divider-last" /> */}
             <footer className="featurette-divider-last">
-                <div className="row">
-                    <div className="col-md-4 col-12">
+                <Row>
+                    <Col md={4}>
                         <h3 className="text-light">
                             Never get lost in the options chain again.
                         </h3>
                         <p className="text-light">
                             Contact us at contact@tigerstance.com or through this <a href="https://forms.gle/qEqcKb1mtG8PJUWq6" target="_blank">google form</a>.
                         </p>
-                    </div>
+                    </Col>
+                    <Col md={{ span: 4, offset: 4 }} style={{paddingTop: '1rem',}}>
+                        <TwitterTimelineEmbed 
+                            sourceType="profile"
+                            screenName="EaseandExtra"
+                            options={{height: 250,width: 300}}
+                            noFooter={true}
+                            theme="dark"
+                            transparent
+                        />
+                    </Col>
                     {/* <div className="col-md-2 col-6">
                         <h3 className="text-light">
                             Learn
@@ -54,29 +67,29 @@ function Footer(props) {
                             PUT EMAIL INPUT HERE
                         </p>
                     </div> */}
-                </div>
+                </Row>
                 <hr />
-                <div className="row">
-                    <div className="col">
+                <Row>
+                    <Col>
                         <p className="text-muted">
                             All content and data on tigerstance.com is for informational purposes only, you should not construe
                             any such information or other material as legal, tax, trading, investment, financial, or other advice.
                         See <Link to="/disclaimer" > full legal disclaimer</Link>.
                     </p>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
                         <p className="text-muted">
                             Data and information on tigerstance.com is provided 'as-is' and solely for informational purposes, not for trading purposes or advice, and is delayed.
                         </p>
-                    </div>
-                </div>
-                <div className="row pb-5">
-                    <div className="col">
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
                         <small className="d-block text-muted">Copyright &copy; 2020-2021 tigerstance.com. All Rights Reserved.</small>
-                    </div>
-                </div>
+                    </Col>
+                </Row>
             </footer>
         </div>
     )
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import { Row, Col } from 'react-bootstrap';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
 import './Home.css';
 
 export default function Home() {
