c6c11d3531a658b8bf0f1cf4e322510e9b93b3fd
ts-bmw <77936839+ts-bmw@users.noreply.github.com>
2021-03-16 23:45:43 -0400

    Updates for an end-to-end health check (#555)
    Co-authored-by: Brian Watson <ts-bmw@users.noreply.github.com>

--- django_apps/tiger/middleware.py
+++ django_apps/tiger/middleware.py
@@ -1,10 +1,10 @@
 import logging
 
-from django.conf import settings
 from django.db import connection
 from django.http import HttpResponse
 
 logger = logging.getLogger('console_info')
+
 
 class HealthCheckMiddleware:
     def __init__(self, get_response):
@@ -12,9 +12,9 @@ class HealthCheckMiddleware:
 
     def __call__(self, request):
         # If this is a not a health check request, just let the request pass through untouched.
-        if request.path_info != '/healthcheck/':
+        if not request.path_info.endswith('/healthcheck/'):
             return self.get_response(request)
-        # Otherwise, intercept the request and perfrom a health check.
+        # Otherwise, intercept the request and perform a health check.
         else:
             healthy = True
 
@@ -29,6 +29,6 @@ class HealthCheckMiddleware:
 
             # Return the health check response.
             if healthy:
-                return HttpResponse(status = 200)
+                return HttpResponse(status=200)
             else:
-                return HttpResponse(status = 500)
+                return HttpResponse(status=500)
