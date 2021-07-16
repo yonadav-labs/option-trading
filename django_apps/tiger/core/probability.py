fe2834e455e1fb1932dd2ea8e5fd000f7a831a19
starovp <53204129+starovp@users.noreply.github.com>
2021-07-16 01:13:11 -0400

    Probability-weighted contract value in range (#1189)
    * Probability-weighted contract value in range
    
    * Update probability.py
    
    * Update probability.py
    
    * Update probability.py
    
    * Update probability.py
    
    * Update test_probability.py
    
    Co-authored-by: starovp <philipp@tigerstance.com>
    Co-authored-by: renou <renou@users.noreply.github.com>

--- django_apps/tiger/core/probability.py
+++ django_apps/tiger/core/probability.py
@@ -1,13 +1,27 @@
+import numpy as np
+
 from scipy.stats import norm
 
 
-def probability_of_price_ranges(current_price, sigma, price_ranges):
+# Get value at expiry of a given contract
+def get_value_at_price(strike, target_price, is_call):
+    if is_call:
+        return max(0, target_price - strike) * 100
+    else:
+        return max(0, strike - target_price) * 100
+
+
+# Get a normal distribution object for use elsewhere
+def get_normal_dist(current_price, sigma):
+    return norm(loc=current_price, scale=sigma * current_price)
+
+
+# Probability of a given set of price ranges given a distribution and a list of price ranges
+def probability_of_price_ranges(normal_dist, price_ranges):
     '''
     Use normal dist. To get probability of price ranges given current price and sigma.
     None can mean 0 or infinite. [(10.0, None)] means above 10, [(None, 10.0)] means below 10.
     '''
-    # Build normal dist
-    normal_dist = norm(loc=current_price, scale=sigma * current_price)
 
     # Dict to store results in, k-v/ price range tuple - probability
     prob_dict = {}
@@ -24,5 +38,34 @@ def probability_of_price_ranges(current_price, sigma, price_ranges):
     return prob_dict
 
 
-def probability_of_price_range(current_price, sigma, start_bound, end_bound):
-    return probability_of_price_ranges(current_price, sigma, [(start_bound, end_bound)])[(start_bound, end_bound)]
+# Probability of a single price range
+def probability_of_price_range(normal_dist, start_bound, end_bound):
+    return probability_of_price_ranges(normal_dist, [(start_bound, end_bound)])[(start_bound, end_bound)]
+
+
+# Probabilistic value of the contract given a distribution, price range, the strike, and the right
+def prob_value_in_price_range(normal_dist, price_lower, price_upper, strike, is_call):
+    # Number of measurements to take within the price range
+    num_measurements = 10
+
+    price_points = [i for i in np.linspace(price_lower, price_upper, num=num_measurements + 1, endpoint=True)]
+    pranges = [(price_points[i], price_points[i + 1]) for i in range(len(price_points) - 1)]
+
+    # Get the probabilities of the constructed ranges
+    range_probs = probability_of_price_ranges(normal_dist, pranges)
+
+    # Iterate over the price levels and their probabilities, 
+    # finding the contract value at each price level, and multiplying it by the probability,
+    # then adding it to the running count
+    prob_weighted_value = 0
+    total_prob = 0.0
+    for price_pair, probability in range_probs.items():
+        prob_value = probability * get_value_at_price(strike, (price_pair[0] + price_pair[0]) / 2.0, is_call)
+        prob_weighted_value += prob_value
+        total_prob += probability
+
+    # We then scale the globally likely value assuming the given price range will be the only outcome
+    certain_prob_weighted_value = prob_weighted_value / total_prob
+
+    # Return both as a tuple
+    return (certain_prob_weighted_value, prob_weighted_value)
--- django_apps/tiger/tests/test_probability.py
+++ django_apps/tiger/tests/test_probability.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from tiger.core.probability import probability_of_price_ranges, probability_of_price_range
+from tiger.core.probability import probability_of_price_ranges, probability_of_price_range, get_normal_dist, \
+    prob_value_in_price_range
 
 
 class TestProbability(TestCase):
@@ -19,8 +20,11 @@ class TestProbability(TestCase):
             (None, 90)
         ]
 
+        # Get our dist
+        normal_dist = get_normal_dist(price, sigma)
+
         # Fetch the probabilities of the price ranges
-        range_probs = probability_of_price_ranges(price, sigma, price_ranges)
+        range_probs = probability_of_price_ranges(normal_dist, price_ranges)
 
         # Should be majority in the middle
         self.assertEqual(
@@ -66,11 +70,60 @@ class TestProbability(TestCase):
 
     def test_price_range_probability(self):
         # Handles sigma > current_price where the lowest possible price is 0.
+
+        # Price and sigma for the dist
+        price = 100
+        sigma = 2.0
+
+        # Get our dist
+        normal_dist = get_normal_dist(price, sigma)
+
         self.assertEqual(
-            probability_of_price_range(current_price=100, sigma=2.0, start_bound=0, end_bound=100),
+            probability_of_price_range(normal_dist=normal_dist, start_bound=0, end_bound=100),
             0.276894946576341
         )
+
         self.assertEqual(
-            probability_of_price_range(current_price=100, sigma=2.0, start_bound=None, end_bound=100),
+            probability_of_price_range(normal_dist=normal_dist, start_bound=None, end_bound=100),
             0.276894946576341
+        )
+
+    # Test the theoretical probability-weighted value of a contract within a price range,
+    # assuming either the global probability of the price range, 
+    # or assuming that the given price range is certain.
+    def test_probable_value_in_price_range(self):
+        # Price and sigma for the dist
+        price = 100
+        sigma = 0.2
+
+        # Target price bounds
+        lower_target_price = 105.0
+        upper_target_price = 115.0
+
+        # Contract details
+        strike = 110.0
+        is_call = True
+
+        # Fetch the normal distribution according to these parameters
+        normal_dist = get_normal_dist(price, sigma)
+
+        # Fetch the certain and global probability weighted values
+        range_certain, range_global = prob_value_in_price_range(
+            normal_dist,
+            lower_target_price,
+            upper_target_price,
+            strike,
+            is_call
+        )
+
+        # Verify the certain and global values
+
+        self.assertAlmostEqual(
+            range_certain,
+            90.86649120270334
+        )
+
+        self.assertAlmostEqual(
+            range_global,
+            15.871320355527391
         )
